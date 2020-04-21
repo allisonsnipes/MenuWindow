5d97bf277cbe4af30513b52d4726894f3bdc8fd2
Allison Snipes <ajsnipes@usfca.edu>
2020-04-20 17:58:21 -0700

    works prints to console need to be able to have selections and user feedback

--- src/Menu.java
+++ src/Menu.java
@@ -5,21 +5,22 @@ public class Menu {
 		Menu menu = new Menu(); // creates an instance of the program
 		
 		//call the methods created below
-		
+		menu.printHeader();
+		menu.showMenu();
 	}
 	
 	//if made private then can only be called within a class always make public
-	//when we print out a menu make a print out header -- header when access menu
+	//when we print out a menu make a print out header to the console -- header when access menu
 	public void printHeader() {
-		System.out.println("---------------------------------");
-		System.out.println("|           Welcome to          |");
-		System.out.println("|        Menu Application       |");
-		System.out.println("---------------------------------");
+		System.out.println("+---------------------------------+");
+		System.out.println("|            Welcome to           |");
+		System.out.println("|         Menu Application        |");
+		System.out.println("+---------------------------------+");
 	}
 	
 	//used to 
 	public void showMenu(){
-		System.out.println("Make a selection");
+		System.out.println("Make a selection:");
 		System.out.println("1)Tell me a color.");
 		System.out.println("2)Tell me a shape.");
 		System.out.println("0)Exit");
